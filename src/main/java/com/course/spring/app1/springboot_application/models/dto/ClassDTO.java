35e6dbdbda3e5ced4902d424f227250acbdf370b
JoseMG-ux <joseutp14@gmail.com>
2025-05-30 09:18:35 -0500

    feat: Creacion de clases DTO

---
+++ src/main/java/com/course/spring/app1/springboot_application/models/dto/ClassDTO.java
@@ -0,0 +1,21 @@
+package com.course.spring.app1.springboot_application.models.dto;
+
+public class ClassDTO {
+
+    private String title;
+    private String user;
+    
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getUser() {
+        return user;
+    }
+    public void setUser(String user) {
+        this.user = user;
+    }
+    
+}
---
+++ src/main/java/com/course/spring/app1/springboot_application/models/dto/ParamDTO.java
@@ -0,0 +1,14 @@
+package com.course.spring.app1.springboot_application.models.dto;
+
+public class ParamDTO {
+    private String information;
+
+    public String getInformation() {
+        return information;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+    
+}
